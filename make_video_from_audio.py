2cef9af34ccc8c8c7c3a5a32f42fca3652cf6cab
illtellyoulater <3078931+illtellyoulater@users.noreply.github.com>
2021-12-12 07:22:35 +0100

    Delete make_video_from_audio.py

--- make_video_from_audio.py
+++
@@ -1,9 +0,0 @@
-import sda
-import scipy.io.wavfile as wav
-from PIL import Image
-va = sda.VideoAnimator(gpu=0)# Instantiate the aminator
-fs, audio_clip = wav.read("example/audio.wav")
-frame = Image.open("example/image.bmp")
-frame = "example/image.bmp"
-vid, aud = va(frame, audio_clip, fs=fs)
-va.save_video(vid, aud, "generated.mp4")
