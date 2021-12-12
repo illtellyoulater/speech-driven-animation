32e77c9ce1d2fa1d0987674568c7b251df1695ce
First Last <redoalf@libero.it>
2021-12-12 07:20:32 +0100

    ok

---
+++ start.py
@@ -0,0 +1,27 @@
+
+import os
+import sda
+import scipy.io.wavfile as wav
+from PIL import Image
+from datetime import datetime
+
+
+os.system("cls")
+print("This will generate a video of a person speaking, based on a single photo of that person and an audio file.\n")
+print("Media should be placed in the /example folder.\n")
+
+print("What's the name of the image file? (e.g. 'person.jpg')")
+image_file_name = input()
+print("What's the name of the audio file? (e.g. 'audio.mp3')")
+audio_file_name = input()
+print("\nWorking...\n")
+
+va = sda.VideoAnimator("grid", gpu=0) # Instantiate the animator(model name("crema" | "grid" | "timit"), gpu=0)
+fs, audio_clip = wav.read("example/" + audio_file_name)
+frame = Image.open("example/" + image_file_name)
+frame = "example/" + image_file_name
+vid, aud = va(frame, audio_clip, fs=fs)
+va.save_video(vid, aud, "generated" +  datetime.now().strftime("%d-%m-%Y--%H-%M-%S") +".mp4")
+
+
+print("Done!\n")
